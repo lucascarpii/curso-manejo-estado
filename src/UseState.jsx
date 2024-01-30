63944bf0e83036ebf7f465c13b2c7551df2de1aa
Lucas Fuentes <lucascarpii2002@gmail.com>
2024-01-30 18:09:22 -0300

    <feat> Ejemplo estados simples React.Component vs. useState

---
+++ src/UseState.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+
+const UseState = ({ name }) => {
+  const [error, setError] = React.useState(false)
+
+  return (
+    <div>
+      <h2>Eliminar { name }</h2>
+
+      <p>Por favor, escribe el código de seguridad</p>
+
+      {error && (
+        <p>Error: el código es incorrecto</p>
+      )}
+
+      <input placeholder="Código de seguridad" />
+      <button onClick={()=> setError(!error)}>Comprobar</button>
+    </div>
+  )
+}
+
+export { UseState }
