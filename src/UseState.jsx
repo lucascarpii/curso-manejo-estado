56e50fea35526b751c39825cd149d6e14c46f971
Lucas Fuentes <lucascarpii2002@gmail.com>
2024-01-31 17:30:01 -0300

    <feat> Ejemplo de estados independientes

--- src/UseState.jsx
+++ src/UseState.jsx
@@ -1,8 +1,12 @@
 import React from "react"
 
+const SECURITY_CODE = 'paradigma'
+
 const UseState = ({ name }) => {
+  const [value, setValue] = React.useState('')
   const [error, setError] = React.useState(false)
   const [loading, setLoading] = React.useState(false)
+
 
   React.useEffect(() => {
     console.log("Empezando el efecto")
@@ -11,7 +15,13 @@ const UseState = ({ name }) => {
       setTimeout(() => {
         console.log("Haciendo la validacion")
 
-        setLoading(false)
+        if(value == SECURITY_CODE) {
+          setError(false)
+          setLoading(false)
+        } else {
+          setError(true)
+          setLoading(false)
+        }
 
         console.log("Terminando la validacion")
       }, 3000)
@@ -33,7 +43,12 @@ const UseState = ({ name }) => {
         <p>Cargando...</p>
       )}
 
-      <input placeholder="Código de seguridad" />
+      <input
+        value={value}
+        onChange={(e) => {
+          setValue(e.target.value)
+        }} 
+        placeholder="Código de seguridad" />
       <button onClick={() => setLoading(true)}>Comprobar</button>
     </div>
   )
