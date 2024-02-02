62db2b8b10b1abe1f4bf66fae344241b33a5e871
Lucas Fuentes <lucascarpii2002@gmail.com>
2024-02-02 15:56:19 -0300

    <feat> Estados declarativos con useReducer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { UseState } from './UseState';
-import { ClassState } from './ClassState';
 
 import './App.css';
+import { UseReducer } from './UseReducer';
 
 function App() {
   return (
     <div className="App">
       <UseState name="Use State" />
-      <ClassState name="Class State" />
+      <UseReducer name="Use Reducer" />
     </div>
   );
 }
